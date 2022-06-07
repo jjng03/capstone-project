3408d2ae60ab4da689c59e4e9588ab9392471cb3
James Jung <jjng930@gmail.com>
2022-06-07 08:55:32 -0500

    created useState,useEffect functions for different categories and displayed images on homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,6 +4,8 @@ function Home() {
     //===== USE STATE FUNCTIONS ===== \\
     // const [movies, setMovies] = useState([]);
     const [popularMovies, setPopularMovies] = useState([]);
+    const [upcomingMovies, setUpcomingMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
 
     //===== API URL ===== \\
     async function getPopularMovies() {
@@ -18,6 +20,33 @@ function Home() {
     useEffect(()=>{
         getPopularMovies();
     }, []);
+
+    async function getUpcomingMovies() {
+        const apiKey = "4464b123b33f0570ee7200292fffe1a2"
+        const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`
+
+        const response = await fetch(url)
+        const data = await response.json()
+        setUpcomingMovies(data.results)
+    }
+
+    useEffect(()=>{
+        getUpcomingMovies();
+    }, []);
+
+    async function getTopRatedMovies() {
+        const apiKey = "4464b123b33f0570ee7200292fffe1a2"
+        const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`
+
+        const response = await fetch(url)
+        const data = await response.json()
+        setTopRatedMovies(data.results)
+    }
+
+    useEffect(()=>{
+        getTopRatedMovies();
+    },[])
+
     // const imageUrl = `https://image.tmdb.org/t/p/w500/${popularMovies.poster_path}`
     // console.log(popularMovies)
     return (
@@ -26,23 +55,46 @@ function Home() {
                 <h1>Hello World!</h1>
             </div>
             <div className="movie-section1">
-                <h1>Popular Movies</h1>
-                {
-                    popularMovies.map((popularMovie) => (
-                        <div className="card-image">
-                            <img src={ `https://image.tmdb.org/t/p/w500${popularMovie.poster_path}` } className="poster"/>
-                        </div>
-                    ))
-                }
+                <div className="category">
+                    <h1>Popular Movies</h1>
+                </div>
                 <div className="cards">
-
+                    {
+                        popularMovies.map((popularMovie) => (
+                            <div className="card-image">
+                                <img src={ `https://image.tmdb.org/t/p/w500${popularMovie.poster_path}` } className="poster"/>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
             <div className="movie-section2">
-                <h1>Section2</h1>
+                <div className="category">
+                    <h1>Upcoming Movies</h1>
+                </div>
+                <div className="cards">
+                    {
+                        upcomingMovies.map((upcomingMovie) => (
+                            <div className="card-image">
+                                <img src={ `https://image.tmdb.org/t/p/w500${upcomingMovie.poster_path}` } className="poster"/>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
             <div className="movie-section3">
-                <h1>Section3</h1>
+                <div className="category">
+                    <h1>Top Rated Movies</h1>
+                </div>
+                <div className="cards">
+                    {
+                        topRatedMovies.map((topRatedMovie) => (
+                            <div className="card-image">
+                                <img src={ `https://image.tmdb.org/t/p/w500${topRatedMovie.poster_path}` } className="poster"/>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
