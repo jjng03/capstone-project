222fdbe9518b6e5d50d37a87d4a867ba1f2dc79f
James Jung <jjng930@gmail.com>
2022-06-08 12:56:46 -0500

    creating the basic layout for the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 // import { Route, Routes } from 'react-router-dom';
-import Home from "./components/Home.js"
+import Home from "./components/Home.js";
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   );
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,13 @@
+// import { Link } from 'react-router-dom';
+
+function Nav() {
+    return (
+        <div className="nav-container">
+            <div className="home-link">Jetflix</div>
+            <div className="sign-link">Sign in</div>
+        </div>
+        
+    )
+}
+
+export default Nav;
