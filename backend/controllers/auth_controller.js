c711fca195da65331ec6442d081e8fa8447dfc09
James Jung <jjng930@gmail.com>
2022-06-09 09:06:16 -0500

    installed cryptojs dependency to hash the password using a secret_key

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const CryptoJS = require('crypto-js');
+
 const router = express.Router();
 const User = require('../models/User');
 
@@ -7,7 +9,7 @@ router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
     });
     
     try {
