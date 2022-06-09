e5d3f3aa51052fd87a88943e498edf1ee0198116
James Jung <jjng930@gmail.com>
2022-06-09 09:17:27 -0500

    created a login route and set conditions  for login

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -20,4 +20,22 @@ router.post('/register', async (req, res) => {
     }
 })
 
+// LOGIN ROUTE
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("Wrong password or username!")
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && 
+            res.status(404).json("Wrong password or username!");
+
+            res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router;
